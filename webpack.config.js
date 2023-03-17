66d2012e1b9f6cf6281840dbc5c613266b3288ae
herakle-dev <ferrigno.ivan98@gmail.com>
2023-03-17 13:24:56 +0100

    path(?)

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
-        }
+        } , include: path.resolve(__dirname, './assets/script'),
       },
       {
         test: /\.html$/,
