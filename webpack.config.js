8909ca06862974356a696a4ca29dea7079bc37b3
herakle-dev <ferrigno.ivan98@gmail.com>
2023-03-18 17:33:35 +0100

    as

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './assets/script',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -16,7 +16,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
-        } , 
+        } , include: path.resolve(__dirname, './assets/script'),
       },
       {
         test: /\.html$/,
@@ -39,7 +39,6 @@ module.exports = {
       template: './src/index.html'
     }),
     new MiniCssExtractPlugin({
-        
       filename:  'style.css'
     })
   ]
